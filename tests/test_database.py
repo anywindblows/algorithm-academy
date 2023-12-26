2bdfbdf5322afab2382cdd8a8c5a9fb21e4703a2
Alexander Kondratev <kondratev.av@inbox.ru>
2023-12-26 21:22:47 +0500

    configure ruff conf

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,7 +4,7 @@ from src.database import resolve_table_name
 
 
 @pytest.mark.parametrize(
-    'input_name, expected_name',
+    ('input_name', 'expected_name'),
     [
         ('Users', 'users'),  # single word
         ('userAccounts', 'user_accounts'),  # camel case
@@ -12,6 +12,7 @@ from src.database import resolve_table_name
         ('customerOrderDetails', 'customer_order_details'),  # mixed case
     ],
 )
-def test_resolve_table_name(input_name, expected_name):
+def test_resolve_table_name(input_name: str, expected_name: str) -> None:
+    """Test resolve_table_name function correct convert string."""
     result = resolve_table_name(input_name)
     assert result == expected_name
